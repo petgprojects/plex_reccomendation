e0a302516200170f902d6ce6e8549b144f1d8a68
Peter Gelgor <peter.gelgor@queensu.ca>
2025-05-14 22:00:00 -0400

    did something

--- webhook.sh
+++ webhook.sh
@@ -5,7 +5,15 @@ set -eu
 SCRIPTS_DIR="$(dirname "$0")"
 echo $SCRIPTS_DIR
 VENV_DIR="$SCRIPTS_DIR/plex_recs_env"
+echo "venv dir: $VENV_DIR"
 REQ_FILE="$SCRIPTS_DIR/requirements.txt"
+echo "req file: $REQ_FILE"
+
+if [ ! -f "$REQ_FILE" ]; then
+  echo "[webhook] ERROR: requirements.txt not found at $REQ_FILE"
+  echo "[webhook]        Did you mount the correct folder into the container?"
+  exit 1
+fi
 
 if [ ! -d "$VENV_DIR" ]; then
     echo "[webhook] Creating venv in $VENV_DIR"
@@ -18,8 +26,13 @@ else
     PYTHON_DIR="$VENV_DIR/bin"
 fi
 
+echo "python dir: $PYTHON_DIR"
+
 STAMP="$VENV_DIR/.__installed"
-if [ "$REQ_FILE" -nt "$STAMP" ]; then
+
+echo "stamp: $STAMP"
+
+if [ ! -f "$STAMP" ] || [ "$REQ_FILE" -nt "$STAMP" ]; then
     echo "[webhook] Installing / updating Python deps …"
     "$PYTHON_DIR/pip" install --no-cache-dir -r "$REQ_FILE"
     touch "$STAMP"
